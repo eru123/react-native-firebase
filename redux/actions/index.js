242c308652794d9b844812a7507ab3d6be58f006
eru123 <yeoligoakino@gmail.com>
2022-02-14 22:06:01 +0800

    remove redux

--- redux/actions/index.js
+++
@@ -1,5 +0,0 @@
-export function fetchUser(){
-  return async (dispatch) => {
-
-  }
-}
--- redux/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducer } from redux
-import { user } from './user'
-const Reducers = combineReducer({
-  userState:user 
-})
-
-export default Reducers
-
-
