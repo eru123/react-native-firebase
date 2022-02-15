0500ac4a5fedacc09e3f30799d8296ce590c4c54
eru123 <yeoligoakino@gmail.com>
2022-02-15 14:27:30 +0800

    implement mobx

--- App.jsx
+++ App.jsx
@@ -1,34 +1,83 @@
-import React, {useEffect} from 'react'
-import {Text} from 'react-native'
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import Landing from './components/auth/Landing';
-// import Register from './components/auth/Register';
-// import Login from './components/auth/Login';
-// import { initializeApp } from 'firebase/app';
-// import { getAuth, onAuthStateChanged } from 'firebase/auth'
-// import cfg from './config.js'
+import React, { useEffect, useState } from 'react'
+import { Text, View, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { observer } from 'mobx-react'
+import tw from 'tailwind-react-native-classnames'
 
-// initializeApp(cfg.firebaseConfig);
+import { userStore } from './store/user'
 
-// const Stack = createNativeStackNavigator();
-// const auth = getAuth()
+import Landing from './components/auth/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import cfg from './config.js'
+const Stack = createNativeStackNavigator();
 
-export default function App() {
-	// useEffect(() => {
-	// 	onAuthStateChanged(auth, user => {
-	// 		console.log(user, 'firebase auth state change')
-	// 	})
-	// })
+const App = observer(() => {
+	const [loaded, setLoaded] = useState(false)
 
-	return (
-		<Text>SS</Text>
-		// <NavigationContainer>
-		// 	<Stack.Navigator initialRouteName="Landing">
-		// 		<Stack.Screen name="App" component={Landing} options={{ headerShown: false }} />
-		// 		{/* <Stack.Screen name="Register" component={Register} /> */}
-		// 		{/* <Stack.Screen name="Login" component={Login} /> */}
-		// 	</Stack.Navigator>
-		// </NavigationContainer>
-	);
-}
+	initializeApp(cfg.firebaseConfig);
+	const auth = getAuth()
+	const email = 'test@mail.com'
+	const pass = '!Password123'	
+
+	const login = async () => {
+		return await signInWithEmailAndPassword(auth, email, pass)
+			.then((e) => console.log('in'))
+			.catch((e) => console.log(e))
+	}
+
+	const register = async () => {
+		return await createUserWithEmailAndPassword(auth, email, pass)
+			.then((e) => console.log('in'))
+			.catch((e) => console.log(e))
+	}
+
+	const logout = async () => {
+		return await signOut(auth)
+	}
+
+	useEffect(() => {
+		onAuthStateChanged(auth, u => {
+			if(!loaded) setLoaded(true)
+			userStore.setUserProvider(u)
+			console.log('firebase auth state change')
+		})
+	},[]);
+
+	useEffect(()=> {
+		console.log(userStore.createMode, 'create mode')
+	},[userStore.createMode])
+
+	if(!loaded) return <View style={tw`flex items-center justify-center`}><Text>Loading...</Text></View>
+
+	// if(!userStore.auth)return (
+	// 	<View style={tw`flex-1 justify-center items-center`}>
+	// 		<Text>{userStore.userProvider?.uid}</Text>
+	// 		<Button title="Login" onPress={() => login()} />
+	// 		<Button title="Register" onPress={() => register()} />
+	// 	</View>
+	// )
+
+	if (!userStore.auth || userStore.createMode) return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Landing">
+				<Stack.Screen name="Landing" component={Landing} options={{ headerShown: false }} />
+				<Stack.Screen name="Register" component={Register} />
+				<Stack.Screen name="Login" component={Login} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
+
+	else return (
+			<View style={tw`flex-1 justify-center items-center`}>
+				<Text>{userStore.userProvider?.uid}</Text>
+				{/* <Button title="Login" onPress={() => login()} /> */}
+				{/* <Button title="Register" onPress={() => register()} /> */}
+				<Button title="Logout" onPress={() => logout()} />
+			</View>
+		)
+})
+export default App
